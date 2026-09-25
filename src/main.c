517ddfdefedc25fb131aa6450dc77cdcaa99b7bf
maukka <markus.veijola@gmail.com>
2026-09-25 11:26:22 +0300

    Clean up

--- src/main.c
+++ src/main.c
@@ -22,13 +22,9 @@ int main(void) {
     ili9341_reset();
 	ili9341_init_display();
 
-	uint8_t s_win = ili9341_set_address_window(0, 0, 239, 319);   // koko rivin leveys
-	uint8_t s_dma = ili9341_write_color_dma(0xF800, 240UL * 320UL);
-	//ili9341_set_address_window(0, 0, 239, 319);   // koko ruutu
-	//ili9341_write_color_dma(0x0000, 240UL * 320UL); 
-	//ili9341_write_color(0x0000, 240UL * 320UL);   // tyhjennä mustaksi
+	uint8_t s_win = ili9341_set_address_window(0, 0, 239, 319);
 
-	ili9341_draw_string(105, 148, "Hello", 0xFFFF, 0x0000);   // valkoinen teksti, musta tausta
+	ili9341_draw_string(105, 148, "Hello", 0xFFFF, 0x0000);
 	ili9341_draw_string(105, 160, "World", 0xFFFF, 0x0000);
 
     while (1) {
